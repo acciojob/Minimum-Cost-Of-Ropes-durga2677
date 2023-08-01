71b50780f9bf7fcc927bd87bba9fc88e987e472a
durga2677 <125868369+durga2677@users.noreply.github.com>
2023-08-01 12:21:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function compare(a, b) {
-  if (a < b) {
-      return -1;
-  } else if (a > b) {
-      return 1;
-  } else {
-      return 0;
-  }
-}
- 
- 
- 
 function calculateMinCost() {
   //your code here
   let str=document.getElementById('rope-lengths').value.split(",");
